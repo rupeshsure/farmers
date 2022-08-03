a1fb069e6e0a811c55d652ef0a740ff7e5f642e6
Rupesh Sure <92075957+rupeshsure@users.noreply.github.com>
2022-08-03 16:45:28 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -58,7 +58,7 @@ disease_classes = ['Apple___Apple_scab',
                    'Tomato___Tomato_mosaic_virus',
                    'Tomato___healthy']
 
-disease_model_path = '/PROJECT 9/app/models/plant_disease_model.pth'
+disease_model_path = 'models/plant_disease_model.pth'
 disease_model = ResNet9(3, len(disease_classes))
 disease_model.load_state_dict(torch.load(
     disease_model_path, map_location=torch.device('cpu')))
@@ -67,7 +67,7 @@ disease_model.eval()
 
 # Loading crop recommendation model
 
-crop_recommendation_model_path = '/PROJECT 9/app/models/RandomForest.pkl'
+crop_recommendation_model_path = 'models/RandomForest.pkl'
 crop_recommendation_model = pickle.load(
     open(crop_recommendation_model_path, 'rb'))
 
